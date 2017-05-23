6725b0387f19de185de8065b554f0b88a9b17973
AndrewBal <balshut@gmail.com>
2017-05-23 20:47:19 +0300

    add map + city_select

--- js/script.js
+++ js/script.js
@@ -4,7 +4,16 @@ $(document).ready(function(){
     slidesToShow: 3,
     slidesToScroll: 1,
      variableWidth: true
-
   });
+  
+    $('#city_select > li').click(function (event) {
+        $(this).children("ul").slideToggle();
+        event.stopPropagation();
+    });
+    $('.bottom_arrow').click(function (event) {
+        $('#city_select > li').children("ul").slideToggle();
+        event.stopPropagation();
+    });
+
 });
 
