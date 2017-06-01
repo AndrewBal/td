397c4792fd09c6d6f218a14de23a78651d5daea7
AndrewBal <balshut@gmail.com>
2017-06-01 23:13:49 +0300

    add_contacts

--- js/script.js
+++ js/script.js
@@ -1,19 +1,24 @@
-$(document).ready(function(){
-  $('.recommend_wrap').slick({
-    centerPadding: '250px', 
-    slidesToShow: 3,
-    slidesToScroll: 1,
-     variableWidth: true
-  });
-  
-    $('#city_select > li').click(function (event) {
+$(document).ready(function() {
+    $('.recommend_wrap').slick({
+        centerPadding: '250px',
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        variableWidth: true
+    });
+
+    $('#city_select > li').click(function(event) {
         $(this).children("ul").slideToggle();
         event.stopPropagation();
     });
-    $('.bottom_arrow').click(function (event) {
+    $('.bottom_arrow').click(function(event) {
         $('#city_select > li').children("ul").slideToggle();
         event.stopPropagation();
     });
 
-});
+    $('.buy_slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+    });
 
+});
